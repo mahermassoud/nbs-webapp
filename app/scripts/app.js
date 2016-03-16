90bcb8466fb3ad1059d9c20e828c021994f69f72
vrynkov <vrynkov@yahoo.com>
2016-03-16 15:46:56 -0700

    Created new repository for NDEx Cravat WebApp. This is just an empty scaffold generated by Yeoman.

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ndexCravatWebappApp
+ * @description
+ * # ndexCravatWebappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ndexCravatWebappApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

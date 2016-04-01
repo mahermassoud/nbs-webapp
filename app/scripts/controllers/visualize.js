9b5f97a4840aabd3e0e7f31345d1165ad1745ce7
vrynkov <vrynkov@yahoo.com>
2016-04-01 11:23:54 -0700

    At this point we can: 1) get a list of networks from cravat 2) select the "View Netwok" link, which will take us to the View screen 3) on the View screen, the ViewCtrl will retrieve the network in CX form from dev2, post it to Kei's service at http://ci-dev-serv.ucsd.edu:3001/cx2cyjs, and get the network back from Kei's service in CYJS format.
    The next step is to visualize the network received from Kei's service in CYJS format.

---
+++ app/scripts/controllers/visualize.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ndexCravatWebappApp.controller:VisualizeCtrl
+ * @description
+ * # VisualizeCtrl
+ * Controller of the ndexCravatWebappApp
+ */
+angular.module('ndexCravatWebappApp')
+  .controller('VisualizeCtrl', function ($routeParams, $http, $scope) {
+
+      var networkUUID = $routeParams.networkUUID;
+
+      console.log('in VisualizeCtrl; network UUID = ' + networkUUID);
+
+
+      $scope.networkAsCX = {};
+      var networkAsCX = $scope.networkAsCX;
+      networkAsCX.network = undefined;
+
+      //var SERVICE_URL = 'http://192.168.99.100:3000/converter/cx2cyjs';
+      var SERVICE_URL = 'http://ci-dev-serv.ucsd.edu:3001/cx2cyjs';
+
+
+      var req = {
+        'method': 'GET',
+        'url': 'http://dev2.ndexbio.org/rest/network/' + networkUUID + '/asCX',
+      };
+
+      $http( req
+
+      ).success(
+
+          function( response ) {
+
+            console.log('got Network as CX success!!!');
+
+            var networkAsCX = response;
+
+            req = {
+              'method': 'POST',
+              'url': SERVICE_URL,
+              data : angular.toJson(networkAsCX),
+
+              'headers': {
+                'Content-Type': 'application/json'
+              }
+            };
+
+            //networkAsCX.network = response;
+
+            $http( req
+
+            ).success(
+
+                function( response ) {
+
+                    console.log('response success from Keis service!!!');
+
+
+                    $scope.visualizeNetwork(response);
+                    //networkAsCX.network = response;
+                }
+
+
+
+            ) .error (
+
+                function( response ) {
+
+                  console.log('response failed from Keis service!!! ' + response);
+
+
+                }
+            );
+
+          }
+
+      ).error (
+
+          function( response ) {
+
+            console.log('response failed!!!' + response);
+
+            delete networkAsCX.network;
+
+          }
+      );
+
+      $scope.visualizeNetwork = function(networkAsCX) {
+         console.log('in visualizeNetwork');
+      };
+
+  });
